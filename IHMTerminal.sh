74c09a25c7fef2b12b95e0874a0d4297eca7ebc0
Igor Stevanato <igorstevanato@gmail.com>
2019-08-03 07:46:10 -0300

    Adicionado arquivo IHMTerminal.sh

---
+++ IHMTerminal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#   IHMTerminal.sh
+#   Programa para ler informações de pontos analógicos e digitais e também efetuar comandos no SAGE
+#
+#   Igor Siqueira Stevanato <igorstevanato@gmail.com>
+#   03/08/2019
+#
+#   Versão 0.1: lê informações de pontos digitais
+#
+
+
+
